fbe92a23d6eaf6e940368530a133eb00b2e1ceaf
Budgie <124753458+BudgieBirdCute@users.noreply.github.com>
2023-07-07 13:09:21 -0600

    Update top.php

--- add-on/top.php
+++ add-on/top.php
@@ -8,5 +8,8 @@
     <meta name="description" content="A website">
     <meta name="author" content="Areeb Islam">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="50">
+    <base href="https://www.budgies.com/" target="_blank">
+    <script src="../func.js"></script>
   </head>
 <body>
