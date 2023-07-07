2a99e47151ed013ffb33ded2ee521cb7b114f76b
Budgie <124753458+BudgieBirdCute@users.noreply.github.com>
2023-07-07 13:05:53 -0600

    Update top.php

--- add-on/top.php
+++ add-on/top.php
@@ -2,5 +2,11 @@
 <html>
   <head>
     <title><?php echo "The Website";?></title>
+    <link rel="stylesheet" href="../style.css">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Website">
+    <meta name="description" content="A website">
+    <meta name="author" content="Areeb Islam">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 <body>
